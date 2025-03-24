bf64ece75defa1084e8c261aa84cb17bfc371f4d
manasa-nagesh <160208382+manasa-nagesh@users.noreply.github.com>
2025-03-24 15:45:18 +0530

    Update even.java

--- even.java
+++ even.java
@@ -2,7 +2,7 @@ public class even {
 
     public static void main(String[] args) {
 
-        int num = 5;
+        int num = 7;
 
         if(num % 2 == 0)
             System.out.println(num + " is even");
